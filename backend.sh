72c4990f18120ac612498631d6b185cae5a4d1fa
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-10-31 11:46:59 +0530

    expense-shell

--- backend.sh
+++ backend.sh
@@ -1,3 +1,91 @@
+# #!/bin/bash
+
+# USERID=$(id -u)
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# LOGS_FOLDER="/var/log/expense-logs"
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+
+# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+# SCRPT_DIR=$PWD
+
+# mkdir -p $LOGS_FOLDER
+# echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# #check the user has root priveleges or not 
+# if [ $USERID -ne 0 ]
+# then 
+#     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+#     exit 1 
+# else    
+#     echo "You are running with root access" | tee -a $LOG_FILE
+# fi
+
+# #validate functions takes input as exit status, what command they tried to install
+# VALIDATE(){
+#     if [ $1 -eq 0 ]
+#     then 
+#         echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+#     else
+#         echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+#         exit 1
+#     fi
+# }
+
+# dnf module disable nodejs -y &>>$LOG_FILE
+# VALIDATE $? "Disabling default nodejs"
+
+# dnf module enable nodejs:20 -y &>>LOG_FILE
+# VALIDATE $? "Enabling nodejs:20"
+
+# dnf install nodejs -y &>>$LOG_FILE
+# VALIDATE $? "Installing nodejs:20"
+
+# id expense
+# if [$? -ne 0 ]
+# then 
+#     useradd --system --home /app --shell /sbin/nologin --comment "expense user" expense &>>$LOG_FILE
+#     VALIDATE $? "Creating expense user"
+# else 
+#     echo -e "System user expense already created ... $Y SKKIPPING $N"
+# fi
+
+# mkdir -p /app
+# VALIDATE $? "Creating app directory"
+
+# curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+# VALIDATE $? "Downloading expense-backend"
+
+# rm -rf /app/*
+# cd /app
+# unzip /tmp/backend.zip &>>$LOG_FILE
+# VALIDATE $? "unzipping backend"
+
+# npm install &>>$LOG_FILE
+# VALIDATE $? "Installing Dependencies"
+
+# cp $SCRPT_DIR/backend.service /etc/systemd/system/backend.service
+# VALIDATE $? "Copying backend service"
+
+# #prepare mysql schema
+
+# dnf install mysql -y &>>$LOG_FILE
+# VALIDATE $? "Installing MySQL Client"
+
+# mysql -h mysql.sree84s.site -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+# VALIDATE $? "Setting up the transactions schema and tables"
+
+# systemctl daemon-reload &>>$LOG_FILE
+# VALIDATE $? "Daemon Reload"
+
+# systemctl enable backend &>>$LOG_FILE
+# VALIDATE $? "Enabling backend"
+
+# systemctl restart backend &>>$LOG_FILE
+# VALIDATE $? "Starting Backend"
+
 #!/bin/bash
 
 USERID=$(id -u)
@@ -5,83 +93,82 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 LOGS_FOLDER="/var/log/expense-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRPT_DIR=$PWD
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
-#check the user has root priveleges or not 
-if [ $USERID -ne 0 ]
-then 
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 
-else    
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
-
-#validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling default nodejs"
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
+}
 
-dnf module enable nodejs:20 -y &>>LOG_FILE
-VALIDATE $? "Enabling nodejs:20"
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing nodejs:20"
+CHECK_ROOT
 
-id expense
-if [$? -ne 0 ]
-then 
-    useradd --system --home /app --shell /sbin/nologin --comment "expense user" expense &>>$LOG_FILE
-    VALIDATE $? "Creating expense user"
-else 
-    echo -e "System user expense already created ... $Y SKKIPPING $N"
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Disabling existing default NodeJS"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling NodeJS 20"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing NodeJS"
+
+id expense &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+    useradd expense &>>$LOG_FILE_NAME
+    VALIDATE $? "Adding expense user"
+else
+    echo -e "expense user already exists ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app
+mkdir -p /app &>>$LOG_FILE_NAME
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
-VALIDATE $? "Downloading expense-backend"
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading backend"
 
-rm -rf /app/*
 cd /app
-unzip /tmp/backend.zip &>>$LOG_FILE
-VALIDATE $? "unzipping backend"
+rm -rf /app/*
 
-npm install &>>$LOG_FILE
-VALIDATE $? "Installing Dependencies"
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "unzip backend"
 
-cp $SCRPT_DIR/backend.service /etc/systemd/system/backend.service
-VALIDATE $? "Copying backend service"
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "Installing dependencies"
 
-#prepare mysql schema
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
 
-dnf install mysql -y &>>$LOG_FILE
+# Prepare MySQL Schema
+
+dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h mysql.sree84s.site -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+mysql -h mysql.sree84s.site -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
 VALIDATE $? "Setting up the transactions schema and tables"
 
-systemctl daemon-reload &>>$LOG_FILE
+systemctl daemon-reload &>>$LOG_FILE_NAME
 VALIDATE $? "Daemon Reload"
 
-systemctl enable backend &>>$LOG_FILE
+systemctl enable backend &>>$LOG_FILE_NAME
 VALIDATE $? "Enabling backend"
 
-systemctl restart backend &>>$LOG_FILE
+systemctl restart backend &>>$LOG_FILE_NAME
 VALIDATE $? "Starting Backend"
